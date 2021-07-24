5eecb37790622ae5ad13c5fef9c1531b20a2287c
cristhianmunozepn <cristhian.munoz@epn.edu.ec>
2021-07-24 02:31:45 -0500

    Se agrega la clase de ejecución. Creación de archivo .jar

---
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -0,0 +1,14 @@
+package ec.edu.epn.git.calculator;
+
+public class CalculatorExecute {
+    public static void  main(String[] args){
+        System.out.println("Ejecución de la Calculadora");
+
+        Calculator c= new Calculator();
+        int addition= c.addition(5,6);
+        System.out.println("c.addition(5,6)"+addition);
+
+        int subtraction= c.subtraccion(8,6);
+        System.out.println("c.subtraccion(8,6)"+subtraction);
+    }
+}
